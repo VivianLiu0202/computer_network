367aa0acbbaa8196fee410c8676d84f1ca51c261
LiuVivian <liuvivian@LiuViviandeMacBook-Pro.local>
2023-10-13 10:44:53 +0800

    make change

--- lab1/client.c
+++ lab1/client.c
@@ -5,55 +5,79 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-#define PORT 8080
+//定义了服务器的端口号和缓冲区大小
+const int PORT= 8080;
 #define BUFFER_SIZE 2048
+//定义客户端client，服务器地址结构和消息缓冲区
+int client_socket;
+struct sockaddr_in server_addr;
 
+//为接收服务器消息的线程设计的。它接受一个socket作为参数。
 void *receive_messages(void *socket) {
     int sock = *(int *)socket;
-    char buffer[BUFFER_SIZE];
-    int read_size;
-
-    while ((read_size = recv(sock, buffer, BUFFER_SIZE, 0)) > 0) {
-        buffer[read_size] = '\0';
-        printf("%s\n", buffer);
+    while(1)
+    {
+        char buffer[BUFFER_SIZE];
+        int read_size=recv(sock, buffer, BUFFER_SIZE, 0);
+        if(read_size<=0)//如果接收到的字符数小于0，就退出
+        {
+            perror("与服务器断开连接\n");
+            exit(EXIT_FAILURE);
+            break;
+        }
+        else{//如果接收到的字符数大于0，就打印出来
+            buffer[read_size] = '\0';
+            printf("%s\n", buffer);
+        }
     }
-
     return NULL;
 }
 
 int main() {
-    int client_socket;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
 
+    printf("客户端启动\n");
+
+    ////==================创建客户端的socket=======================
+    //创建一个新的socket。如果创建失败，打印错误消息并退出程序
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1) {
-        perror("Could not create socket");
-        exit(EXIT_FAILURE);
+        perror("创建socket: failed\n");
+        return -1;
     }
+    printf("创建socket:%d success\n",client_socket);
 
+    //=========================服务器地址=========================
+    //设置服务器地址结构。这里，客户端尝试连接到本地地址127.0.0.1和之前定义的端口号
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
 
+    //=========================连接到服务器=========================
+    //连接到服务器。如果连接失败，打印错误消息并退出程序
     if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connect failed");
+        perror("connect: failed\n");
         exit(EXIT_FAILURE);
     }
+    printf("Connect to the server.Type messages...\n");
 
-    printf("Connected to server. Type messages...\n");
-
+    //=========================创建线程=========================
+    //创建一个新的线程来接收服务器的消息。如果创建线程失败，打印错误消息并退出程序。
     pthread_t receive_thread;
     if (pthread_create(&receive_thread, NULL, receive_messages, (void *)&client_socket) < 0) {
-        perror("Could not create thread");
+        perror("创建线程: failed\n");
         exit(EXIT_FAILURE);
     }
+    printf("创建线程: success\n");
 
+    //=========================发送消息=========================
+    //持续从标准输入读取消息并发送给服务器，如果输入EXIT就退出循环
     while (1) {
+        char buffer[BUFFER_SIZE] = {0};
         fgets(buffer, BUFFER_SIZE, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';  // Remove newline
         send(client_socket, buffer, strlen(buffer), 0);
         if (strcmp(buffer, "EXIT") == 0) {
+            printf("退出聊天室\n");
             break;
         }
     }
--- lab1/server.c
+++ lab1/server.c
@@ -5,14 +5,23 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <errno.h>
 
-#define PORT 8080 // 服务器监听的端口号
+const int PORT=8080; // 服务器监听的端口号
 #define BUFFER_SIZE 2048 //消息缓冲区大小
 //缓冲区用来存放客户端发来的消息，每个客户端对应一个缓冲区
 #define MAX_CLIENTS 4 //最大客户端数量
 
 int client_sockets[MAX_CLIENTS];// client_sockets存储所有连接的客户端的套接字
 int client_count = 0;//使用client_count来跟踪当前连接的客户端数量
+int client_sockets_status[MAX_CLIENTS];//记录当前的socket数组位置是否为空
+
+int client_status(){
+    for(int i=0;i<MAX_CLIENTS;i++){
+        if(client_sockets_status[i] == 0) return i;
+    }
+    return -1;
+}
 
 void add_info(char* message){
     time_t rawtime;
@@ -34,41 +43,67 @@ void add_info(char* message){
     //将带有时间戳的消息复制回原来的消息变量
     strcpy(message, time_stamp);
 }
+
 //一个线程函数，用于处理客户端的消息
-void *handle_client(void *client_socket) {
+void *handle_client(void *client_socket) 
+{
     int sock = *(int *)client_socket; //获取客户端的socket
-    char buffer[BUFFER_SIZE]; //定义字符数组buffer存储接收到的消息
+    char Recvbuffer[BUFFER_SIZE]; //接受消息的缓冲区
+    char Sendbuffer[BUFFER_SIZE]; //发送消息的缓冲区
     int read_size; //存储接收到的消息的大小
 
-    //接收客户端的消息并转发给其他客户端
-    while ((read_size = recv(sock, buffer, BUFFER_SIZE, 0)) > 0) {
-        buffer[read_size] = '\0';//消息字符串以空字符结尾
+    while((read_size=recv(sock, Recvbuffer, BUFFER_SIZE, 0))>0)
+    {
+        //接收客户端的消息并转发给其他客户端
+        Recvbuffer[read_size] = '\0';//消息字符串以空字符结尾
         //如果客户端发来的消息是EXIT，则退出
-        if (strcmp(buffer, "EXIT") == 0) {
+        if (strcmp(Recvbuffer, "EXIT") == 0) {
+            read_size=0;
             break;
         }
-        add_info(buffer);
-        //将消息转发给其他客户端
-        for (int i = 0; i < client_count; i++) {
-            if (client_sockets[i] != sock) {
-                send(client_sockets[i], buffer, strlen(buffer), 0);
-            }
-        }
-    }
+        //将消息添加时间戳
+        add_info(Recvbuffer);
+        printf("client %d: %s \n", sock ,Recvbuffer);
+        sprintf(Sendbuffer, "client %d: %s", sock,Recvbuffer);
 
-    //客户端退出，从客户端列表中删除，从client_sockets数组中移除该客户端的套接字，并更新client_count。
-    for (int i = 0; i < client_count; i++) {
-        if (client_sockets[i] == sock) {
-            for (int j = i; j < client_count - 1; j++) {
-                client_sockets[j] = client_sockets[j + 1];
+        //====================== 向其他socket都发送消息 =====================
+        for (int i = 0; i < client_count; i++) {
+            if (client_sockets_status[i] == 1) {
+                send(client_sockets[i], Sendbuffer, strlen(Sendbuffer), 0);
             }
-            client_count--;
-            break;
+        } 
+    }     
+    if(read_size == 0){
+        for(int i=0;i<client_count;i++){
+            if(client_sockets[i]==sock) {
+                time_t rawtime;
+                struct tm * timeinfo;
+                char time_stamp[80];
+
+                //获取当前时间
+                time(&rawtime);
+                timeinfo = localtime(&rawtime);
+
+                //格式化时间
+                strftime(time_stamp, sizeof(time_stamp), "%Y-%m-%d %H:%M:%S", timeinfo);
+                printf("Client %d exit, number of current clients: %d. TIME: %s \n", sock, client_count-1, time_stamp);
+                client_sockets_status[i]=0;
+                client_count--;
+
+                //关闭客户端的socket，释放分配的内存，并结束线程
+                close(sock);
+                free(client_socket);
+                return NULL;
+            }
         }
     }
-    //关闭客户端的socket，释放分配的内存，并结束线程
-    close(sock);
-    free(client_socket);
+    if(read_size == -1)
+    {
+        perror("recv failed");
+        close(sock);
+        free(client_socket);
+        return NULL;
+    }
     return NULL;
 }
 
@@ -79,41 +114,58 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
 
+    printf("服务器启动\n");
+
     //创建一个新的socket来监听客户端的连接请求。如果创建失败，则打印错误消息并退出程序。
     server_socket = socket(AF_INET, SOCK_STREAM, 0);//创建一个TCP套接字,AF_INET表示IPv4
     if (server_socket == -1) {
-        perror("Could not create socket");
+        perror("创建socket: failed!\n");
         exit(EXIT_FAILURE);
     }
+    printf("创建socket: success!\n");
 
+    //=====================初始化服务器地址============================
     //设置服务器地址结构体的属性。这里，服务器监听所有可用的接口上的指定端口。
     server_addr.sin_family = AF_INET;//AF_INET表示IPv4
     server_addr.sin_addr.s_addr = INADDR_ANY;//INADDR_ANY表示服务器监听所有可用的接口上的指定端口
     server_addr.sin_port = htons(PORT);//htons()函数将主机字节序转换为网络字节序,PORT为服务器监听的端口号,htons(PORT)是将PORT转换为网络字节序
 
+    //=====================bind=============================
     //将socket绑定到指定的地址和端口。如果绑定失败，则打印错误消息并退出程序
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
+    int Bindtemp = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (Bindtemp < 0) {
+        perror("Bind: failed\n");
         exit(EXIT_FAILURE);
     }
+    printf("Bind: bind to port %d success!\n",PORT);
 
+    //=========================listen=========================
     //开始监听客户端的连接请求，并打印一条消息表示服务器已启动。
-    listen(server_socket, 5);
-    printf("Server started. Waiting for connections...\n");
+    int ListenTemp = listen(server_socket, MAX_CLIENTS);
+    if(ListenTemp != 0){
+        perror("listen: failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("listen: success!\n");
+    printf("Server started. Waiting for clients connections...\n\n");
 
+    //=========================接收client=======================
     //设置addr_size为sockaddr_in结构体的大小，这将在接受新的客户端连接时使用
     addr_size = sizeof(struct sockaddr_in);
-    //用于持续接受来自客户端的连接请求。当有新的客户端尝试连接时，accept函数会返回一个新的套接字描述符
+    //用于持续接受来自client的连接请求。当有新的client尝试连接时，accept函数会返回一个新的socket
     while ((new_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size))) {
         if (client_count >= MAX_CLIENTS) {//如果客户端数量超过最大值，则拒绝连接
-            printf("Max clients reached. Connection rejected.\n");
+            printf("连接失败: client数量达到最大 \n");
             close(new_socket);
             continue;
         }
+        int temp = client_status();//返回数组中的空位；
+        client_sockets_status[temp] = 1;
+        client_sockets[client_count] = new_socket;
+        client_count++;
         //打印一条消息，显示已接受的客户端的IP地址和端口号。
-        printf("Connection accepted from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        //将新的socket加入client_sockets数组
-        client_sockets[client_count++] = new_socket;
+        printf("成功连接 %s:%d clients(%d)\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),client_sockets[client_count-1]);
+        printf("Now the number of clients is: %d\n",client_count);
 
         //新建线程，并为新的socket分配内存
         pthread_t client_thread;
@@ -122,9 +174,10 @@ int main() {
 
         //创建一个新的线程来处理新的客户端的消息。如果创建线程失败，则打印错误消息并退出程序。
         if (pthread_create(&client_thread, NULL, handle_client, (void *)new_sock) < 0) {
-            perror("Could not create thread");
+            perror("创建线程: failed!\n");
             exit(EXIT_FAILURE);
         }
+        printf("创建线程: success!\n");
     }
 
     //检查accept函数是否出现错误。如果出现错误，则打印错误消息并退出程序
@@ -132,7 +185,7 @@ int main() {
         perror("Accept failed");
         exit(EXIT_FAILURE);
     }
-
     close(server_socket);
+
     return 0;
 }
